be334546007774b641d4a1c9eff1a56ae6500306
ARAFATH-SH <alarafath.info@gmail.com>
2025-04-19 01:07:26 +0600

    Create Splash and Login class'

---
+++ src/electricity/billing/system/Login.java
@@ -0,0 +1,16 @@
+package electricity.billing.system;
+
+import javax.swing.*;
+
+public class Login extends JFrame {
+
+    Login() {
+        setSize(600, 400);
+        setLocation(500,200);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Login();
+    }
+}
